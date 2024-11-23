b0c332b7b40c13c2f20483782f0bf47200ad5f1a
naredo <nachonaredobernardos@gmail.com>
2024-11-23 10:09:06 +0100

    feat: Added search

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import catalog_view, checkout_view, home_view, product_info
+from .views import catalog_view, checkout_view, home_view, product_info, search_product
 
 app_name = 'product'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', home_view, name='home'),  # URL de la página principal
     path('catalogo/', catalog_view, name='catalog'),  # URL del catálogo
     path('<int:pk>/', product_info, name='product_info'),  # URL de la información del producto
+    path('search/', search_product, name='search_product'),
 ]
