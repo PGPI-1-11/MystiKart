72e45e64e2a0f6e9e182da397031f7a9f7276e30
naredo <nachonaredobernardos@gmail.com>
2024-11-25 11:15:34 +0100

    fix: delete class view

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import catalog_view, checkout_view, home_view, product_info, search_product
+from .views import  checkout_view, home_view, product_info, search_product
 
 
 
@@ -8,7 +8,6 @@ app_name = 'product'
 
 urlpatterns = [
     path('', home_view, name='home'),  # URL de la página principal
-    path('catalogo/', catalog_view, name='catalog'),  # URL del catálogo
     path('<int:pk>/', product_info, name='product_info'),  # URL de la información del producto
     path('search/', search_product, name='search_product'),
 ]
