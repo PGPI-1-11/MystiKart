f1d03ce99338b05e7b371a9efde070e73e57f51d
pabespnar <pabespnar@alum.us.es>
2024-11-29 17:10:23 +0100

    feat: Settings para correo actualizado

--- MystiKart/settings.py
+++ MystiKart/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'MystiKart.wsgi.application'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mystikartpgpi@gmail.com'
-EMAIL_HOST_PASSWORD = 'qsld ftwl epjn pxmc '
+EMAIL_HOST_PASSWORD = 'sqsb fmwc dzyk xqat'
+EMAIL_USE_TLS = True
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
